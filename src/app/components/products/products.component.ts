d040b62bd67a2212f977e7d31c55950b1cd731b2
Benedicte Debruyne <bdebruyne@gmail.com>
2019-05-27 17:00:00 +0200

    Commit de la refactorisation pour utiliser des service

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/domain/iproduct';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-  products: string[] = ['Café', 'Thé', 'Jus d\'orange', 'Coca-Cola'];
-  products2: IProduct[] = [
-    { id: 'P100', name: 'Café', unitPrice: 2.5 },
-    { id: 'P200', name: 'Thé', unitPrice: 3.5 },
-    { id: 'P300', name: 'Jus d\'orange', unitPrice: 1.5 },
-    { id: 'P400', name: 'Coca-Cola', unitPrice: 4.5 }
-  ];
+export class ProductsComponent implements OnInit {
+  products: IProduct[];
 
-  constructor() { }
+  constructor(private service: ProductsService) { }
+
+  ngOnInit(): void { // Lifecycle method that runs immediatly after the constructor
+    this.products = this.service.getAllProducts();
+  }
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from '../domain/iproduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor() { }
+
+  public getAllProducts(): IProduct[] {
+    return [
+      { id: 'P100', name: 'Café', unitPrice: 2.5 },
+      { id: 'P200', name: 'Thé', unitPrice: 3.5 },
+      { id: 'P300', name: 'Jus d\'orange', unitPrice: 1.5 },
+      { id: 'P400', name: 'Coca-Cola', unitPrice: 4.5 }
+    ];
+  }
+}
